8469bcfff2878ea234104f57b5857079d61c4ecd
jlbm1974 <30827640+jlbm1974@users.noreply.github.com>
2022-05-19 12:38:35 +0200

    Update Envision-PostInstallation-Tasks.ps1

--- artifacts/axrminfra/st/scripts/Envision-PostInstallation-Tasks.ps1
+++ artifacts/axrminfra/st/scripts/Envision-PostInstallation-Tasks.ps1
@@ -231,6 +231,9 @@ $scripBlock = @'
 	} else {
 		Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 	}	
+	
+	#Manually Added
+        & 'C:\AgileXRM\gpo\DeployGPOLocally.bat'
 	Stop-Transcript
 '@
 
